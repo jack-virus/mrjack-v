50bd9f52b69638f9f711c139ab662f5c96331e1b
mrjack9722 <51514680+mrjack9722@users.noreply.github.com>
2019-06-08 19:12:27 +0200

    Add files via upload

---
+++ mrjack.py
@@ -0,0 +1,13 @@
+print("ډٱډ جُـ‘ـُٱكُـ‘ـُ  ٱبُـ‘ـُنُـ‘ـُ رٱيسُـ‘ـُنُـ‘ـُډ")
+print("choose frome the list")
+print("1)android 2)micro 3)iphone 4)contact")
+x = int(input("Please enter an integer: "))
+if x == 1:
+    print("https://mega.nz/#F!nboHVaAC!GE-zyldXCcfCTuN_HxY-mA")
+elif x == 2:
+    print("https://mega.nz/#F!yPAwUQ7A!GIcegjdTV2aCytOeDhs20A")
+elif x == 3:
+    print("https://mega.nz/#F!efAAxAbT!kThDdTZ5cqu3fp7yipKMQg")
+
+elif x == 4:
+    print("https://mega.nz/#F!vLZBCAzY!0MN3WfZf6i4B8qPZKjdsTw")
